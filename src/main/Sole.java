cc231e8759b7f7255f702ff8741ba665e66c16b8
ffmaer <ffmaer@gmail.com>
2010-12-14 02:28:49 -0500

    late night commit

--- src/main/Sole.java
+++ src/main/Sole.java
@@ -1,14 +1,13 @@
 package main;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 
 public class Sole {
 	public static void main(String args[]) throws IOException, InterruptedException
 	{
-		int b = 32;
+		int b = 128;
 		BigInteger n = (BigInteger.valueOf(2).pow(b/2)).add(BigInteger.valueOf(1/2));
 		BigInteger blockSize = BigInteger.valueOf(2).pow(b);
 		BigInteger local[] = new BigInteger[4];
@@ -17,19 +16,16 @@ public class Sole {
         try {
             in = new FileInputStream("nyu.png");
             int i = 0;
-            String bin, buffer = "";
+            String bin, buffer;
             bin = get3Buffers("", in, b);
         	while(bin.length() > 3 * b) {
-        		buffer = bin.substring(0,64);
-        		if(buffer.length() == 2 * b) {
-        			actualComp(buffer, b, local, blockSize, i);
-        			i++;
-        			buffer = "";
-        			bin = get3Buffers(bin.substring(2 * b + 1), in, b);
-        			System.out.println(2 * b);
-        		}
+        		
+        		buffer = bin.substring(0, 2 * b);
+        		actualComp(buffer, b, local, blockSize, i);
+        		i++;
+        		bin = get3Buffers(bin.substring(2 * b), in, b);
         	}
-        	
+        	//will handle the EOF here
         	System.out.println(bin);
         	//Thread.sleep(10);		
         }
@@ -38,14 +34,6 @@ public class Sole {
                 in.close();
             }
         }
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 	public static String get3Buffers(String more, FileInputStream in, int b) throws IOException {
 		int c = in.read();
@@ -62,8 +50,8 @@ public class Sole {
 	}
 	public static void actualComp(String buffer,int b, BigInteger[] local, BigInteger blockSize, int i) {
 		String buffer1 = buffer.substring(0, b);
-		String buffer2 = buffer.substring(b, 2 * b) ;
-		if(local[0] == null)
+		String buffer2 = buffer.substring(b);
+		if(local[0] == null)//compute the first output
 		{
 			local[0] = new BigInteger(buffer1,2);
 			local[1] = new BigInteger(buffer2,2);
@@ -75,14 +63,21 @@ public class Sole {
 			Api = blockSize;
 			Bpi = blockSize.add(BigInteger.valueOf(8));
 			result = swap(A,B,Api,Bpi,x,y)[0];
-			System.out.print(result.toString(16));
-			System.out.print(" ");
+			
+//			System.out.print(result.toString(16));
+//			System.out.print(" ");
 		}
-		else if(local[2] == null)
+		else if(local[2] == null)//compute the 2nd and 3rd output
 		{
 			local[2] = new BigInteger(buffer1,2);
 			local[3] = new BigInteger(buffer2,2);
-			compOut(blockSize,local,i);
+			
+			BigInteger[] xypi = compOut(blockSize,local,i);
+			
+//			System.out.print(xypi[0].toString(16));
+//			System.out.print(" ");
+//			System.out.print(xypi[1].toString(16));
+//			System.out.print(" ");
 		}
 		else
 		{
@@ -90,12 +85,17 @@ public class Sole {
 			local[1] = local[3];
 			local[2] = new BigInteger(buffer1,2);
 			local[3] = new BigInteger(buffer2,2);
-			compOut(blockSize,local,i);
+			
+			BigInteger[] xypi = compOut(blockSize,local,i);
+			
+//			System.out.print(xypi[0].toString(16));
+//			System.out.print(" ");
+//			System.out.print(xypi[1].toString(16));
+//			System.out.print(" ");
 		}
 	}
-	public static void compOut(BigInteger blockSize, BigInteger[] local, int i){
+	public static BigInteger[] compOut(BigInteger blockSize, BigInteger[] local, int i){
 		BigInteger A, B, x, y, Api, Bpi, nextx, nexty;
-		BigInteger[] xypi = new BigInteger[2];
 		//1 pass
 		A = blockSize.add(BigInteger.valueOf(2));
 		B = blockSize.add(BigInteger.valueOf(2));
@@ -111,7 +111,6 @@ public class Sole {
 		Api = blockSize.add(BigInteger.valueOf(-(i-0)*8));
 		Bpi = blockSize.add(BigInteger.valueOf(+(i+1)*8));
 		nexty = swap(A,B,Api,Bpi,x,y)[0];
-		
 		//2 pass
 		x = nextx;
 		y = nexty;
@@ -119,11 +118,7 @@ public class Sole {
 		B = blockSize.add(BigInteger.valueOf(-(i-0)*8));
 		Api = blockSize;
 		Bpi = blockSize;
-		xypi = swap(A,B,Api,Bpi,x,y);
-		System.out.print(xypi[0].toString(16));
-		System.out.print(" ");
-		System.out.print(xypi[1].toString(16));
-		System.out.print(" ");
+		return swap(A,B,Api,Bpi,x,y);
 	}
 	public static BigInteger[] swap(BigInteger A,BigInteger B,BigInteger Api,BigInteger Bpi,BigInteger x,BigInteger y){
 		BigInteger z = y.multiply(A).add(x);
